33eca26acc20de6cea175ffa01d5c5c9f8935485
Mark S <marksherm04@gmail.com>
2021-06-09 17:38:04 -0400

    added to local storage set function, put place holder for function timeBlock and if else if else statement

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,22 +2,53 @@
 const todayDateTime = moment().format('LLLL');
 $("#currentDay").html(todayDateTime);
 
-// var text = 
-// var time = 
-// function saveBtn () {
+//function for colors to change with time block
+
+function timeBlock() {
+   let currentTime = moment().hour();
+   let hourlyBlock = " ";
+
+    // if 
+        // .addClass
+        // .removeClass
+        // .removeClass
+    
+    // else if 
+        // .addClass
+        // .removeClass
+        // .removeClass
+    
+    // else 
+        // .addClass
+        // .removeClass
+        // .removeClass
+
+}
 
 
-// localStorage.setItem("time", "btnClickSave");
-// console.log(localStorage);
-// }
 
-localStorage.getItem("#8AM");
-localStorage.getItem("#9AM");
-localStorage.getItem("10AM");
-localStorage.getItem("#11AM");
-localStorage.getItem("#12PM");
-localStorage.getItem("#1PM");
-localStorage.getItem("#2PM");
-localStorage.getItem("#3PM");
-localStorage.getItem("#4PM");
-localStorage.getItem("#5PM");
+// local storage set and get 
+
+//local storage set function
+$(document).ready(function() {
+    $(".saveBtn").click(function() {
+        let text = (".description");
+        let time = ("id");
+
+        localStorage.setItem(time, text);
+    })
+});
+
+// get from local storage
+$("8AM .description").val(localStorage.getItem("#8AM"));
+$("9AM .description").val(localStorage.getItem("#9AM"));
+$("10AM .description").val(localStorage.getItem("10AM"));
+$("11AM .description").val(localStorage.getItem("#11AM"));
+$("12PM .description").val(localStorage.getItem("#12PM"));
+$("1PM .description").val(localStorage.getItem("#1PM"));
+$("2PM .description").val(localStorage.getItem("#2PM"));
+$("3PM .description").val(localStorage.getItem("#3PM"));
+$("4PM .description").val(localStorage.getItem("#4PM"));
+$("5PM .description").val(localStorage.getItem("#5PM"));
+
+timeBlock();
