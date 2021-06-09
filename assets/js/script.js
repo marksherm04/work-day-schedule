73335811b40131d21edec8100bae1ab9c2736e3a
Mark S <marksherm04@gmail.com>
2021-06-09 14:32:11 -0400

    adjusted div classes so buttons stack on each other as well as lines over hours, added localstorage getItem

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,23 @@
 // Display current date
-var todayDateTime = moment().format('LLLL');
+const todayDateTime = moment().format('LLLL');
 $("#currentDay").html(todayDateTime);
 
+// var text = 
+// var time = 
+// function saveBtn () {
+
+
+// localStorage.setItem("time", "btnClickSave");
+// console.log(localStorage);
+// }
+
+localStorage.getItem("#8AM");
+localStorage.getItem("#9AM");
+localStorage.getItem("10AM");
+localStorage.getItem("#11AM");
+localStorage.getItem("#12PM");
+localStorage.getItem("#1PM");
+localStorage.getItem("#2PM");
+localStorage.getItem("#3PM");
+localStorage.getItem("#4PM");
+localStorage.getItem("#5PM");
