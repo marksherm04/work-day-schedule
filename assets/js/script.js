21c011500d0b5b29d23e66b039825dbd38c0000b
Mark S <marksherm04@gmail.com>
2021-06-09 18:56:20 -0400

    commented sections I need to fix for it to run

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,26 +4,40 @@ $("#currentDay").html(todayDateTime);
 
 //function for colors to change with time block
 
-function timeBlock() {
+function timeBlock(element) {
+    // compare element time to current time
+
+    // get current hour (military time)
    let currentTime = moment().hour();
-   let hourlyBlock = " ";
 
-    // if 
-        // .addClass
-        // .removeClass
-        // .removeClass
-    
-    // else if 
-        // .addClass
-        // .removeClass
-        // .removeClass
-    
-    // else 
-        // .addClass
-        // .removeClass
-        // .removeClass
+   // get hour of element (get id value)
+   // example: diveid = 11-hour
+   // get number before '-hour'
 
-}
+   // TODO:
+
+   let hourlyBlock = (time-block id);
+    console.log(currentTime, hourlyBlock);
+
+
+    // compae hourly block to currenTime
+    // set class based on results
+    if (hourlyBlock < currentTime) {
+        $(this).addClass("past");
+        $(this).removeClass("present");
+        $(this).removeClass("future");
+    }
+    else if (hourlyBlock === currentTime) {
+        $(this).addClass("present");
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+    }
+    else if (hourlyBlock > currentTime) {
+        $(this).addClass("future");
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+    }
+};
 
 
 
@@ -31,24 +45,34 @@ function timeBlock() {
 
 //local storage set function
 $(document).ready(function() {
-    $(".saveBtn").click(function() {
-        let text = (".description");
-        let time = ("id");
+    $("#saveBtn").on("click", function() {
+        let text = $(this).siblings(".description").val();
+        let time = $(this).siblings("id").val();
 
         localStorage.setItem(time, text);
     })
 });
 
 // get from local storage
-$("8AM .description").val(localStorage.getItem("#8AM"));
-$("9AM .description").val(localStorage.getItem("#9AM"));
-$("10AM .description").val(localStorage.getItem("10AM"));
-$("11AM .description").val(localStorage.getItem("#11AM"));
-$("12PM .description").val(localStorage.getItem("#12PM"));
-$("1PM .description").val(localStorage.getItem("#1PM"));
-$("2PM .description").val(localStorage.getItem("#2PM"));
-$("3PM .description").val(localStorage.getItem("#3PM"));
-$("4PM .description").val(localStorage.getItem("#4PM"));
-$("5PM .description").val(localStorage.getItem("#5PM"));
+$("#8AM .description").val(localStorage.getItem("#8AM"));
+$("#9AM .description").val(localStorage.getItem("#9AM"));
+$("#10AM .description").val(localStorage.getItem("10AM"));
+$("#11-hour .description").val(localStorage.getItem("11-hour"));
+$("#12PM .description").val(localStorage.getItem("#12PM"));
+$("#1PM .description").val(localStorage.getItem("#1PM"));
+$("#2PM .description").val(localStorage.getItem("#2PM"));
+$("#3PM .description").val(localStorage.getItem("#3PM"));
+$("#4PM .description").val(localStorage.getItem("#4PM"));
+$("#5PM .description").val(localStorage.getItem("#5PM"));
 
-timeBlock();
+// timeBlock();
+
+// LOOP THROUGH ALL ELEMENTS WHERE CLASS
+// = "row time-block"
+
+// Then pass that elelment to function:
+function loopTimeElements() {
+    for (i = 8; i <  18; i ++) {
+        // TODO: get div id for all elements with class = "row time-block"
+    }
+};
