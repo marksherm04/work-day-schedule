aaf390606a2cfbc58b7084fc7dd0aa04e829cacb
Mark S <marksherm04@gmail.com>
2021-06-10 17:39:35 -0400

    added for loop before the if else if else statement to help the hourly block loop through and split the hours, changed $ to jQuery as I just learned that to help me keep track of how it works, console.log for hour id's are populating and showing 10 hours in the array, need to figure out to how to get the colors to show and local storage to save now

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,78 +1,76 @@
 // Display current date
-const todayDateTime = moment().format('LLLL');
-$("#currentDay").html(todayDateTime);
+var todayDateTime = moment().format('LLLL');
+jQuery("#currentDay").html(todayDateTime);
 
-//function for colors to change with time block
 
-function timeBlock(element) {
+
+
+function workSchedule(element) {
     // compare element time to current time
 
     // get current hour (military time)
-   let currentTime = moment().hour();
+    var currentTime = moment().hour();
 
-   // get hour of element (get id value)
-   // example: diveid = 11-hour
-   // get number before '-hour'
-
-   // TODO:
-
-   let hourlyBlock = (time-block id);
+    // get hour of element (get id value)
+    var hourlyBlock = jQuery(".time-block");
     console.log(currentTime, hourlyBlock);
+    // example: divid = 11-hour
+    // get number before '-hour'
 
-
-    // compae hourly block to currenTime
-    // set class based on results
-    if (hourlyBlock < currentTime) {
-        $(this).addClass("past");
-        $(this).removeClass("present");
-        $(this).removeClass("future");
-    }
-    else if (hourlyBlock === currentTime) {
-        $(this).addClass("present");
-        $(this).removeClass("past");
-        $(this).removeClass("future");
-    }
-    else if (hourlyBlock > currentTime) {
-        $(this).addClass("future");
-        $(this).removeClass("past");
-        $(this).removeClass("present");
+    // TODO:
+    for (var i = 0; i < hourlyBlock.length; i++) {
+        let timeSlot = hourlyBlock[i];
+        // compare hourly block to currentTime
+        // set class based on results
+        if (parseInt(timeSlot.id.split(" ")[0]) < currentTime) {
+            jQuery(this).addClass("past");
+        }
+        else if (parseInt(timeSlot.id.split(" ")[0]) === currentTime) {
+            jQuery(this).removeClass("past");
+            jQuery(this).addClass("present");
+        }
+        else {
+            jQuery(this).removeClass("past");
+            jQuery(this).removeClass("present");
+            jQuery(this).addClass("future");
+        }
     }
 };
 
-
+workSchedule();
 
 // local storage set and get 
-
 //local storage set function
-$(document).ready(function() {
-    $("#saveBtn").on("click", function() {
-        let text = $(this).siblings(".description").val();
-        let time = $(this).siblings("id").val();
+jQuery(document).ready(function () {
+    jQuery("#saveBtn").on("click", function () {
+        var text = jQuery(this).siblings(".description").val();
+        var time = jQuery(this).parent().attr("id");
 
         localStorage.setItem(time, text);
     })
 });
 
 // get from local storage
-$("#8AM .description").val(localStorage.getItem("#8AM"));
-$("#9AM .description").val(localStorage.getItem("#9AM"));
-$("#10AM .description").val(localStorage.getItem("10AM"));
-$("#11-hour .description").val(localStorage.getItem("11-hour"));
-$("#12PM .description").val(localStorage.getItem("#12PM"));
-$("#1PM .description").val(localStorage.getItem("#1PM"));
-$("#2PM .description").val(localStorage.getItem("#2PM"));
-$("#3PM .description").val(localStorage.getItem("#3PM"));
-$("#4PM .description").val(localStorage.getItem("#4PM"));
-$("#5PM .description").val(localStorage.getItem("#5PM"));
+
+jQuery("#8-hour .description").val(localStorage.getItem("8-hour"));
+jQuery("#9-hour .description").val(localStorage.getItem("9-hour"));
+jQuery("#10-hour .description").val(localStorage.getItem("10-hour"));
+jQuery("#11-hour .description").val(localStorage.getItem("11-hour"));
+jQuery("#12-hour .description").val(localStorage.getItem("12-hour"));
+jQuery("#13-hour .description").val(localStorage.getItem("13-hour"));
+jQuery("#14-hour .description").val(localStorage.getItem("14-hour"));
+jQuery("#15-hour .description").val(localStorage.getItem("15-hour"));
+jQuery("#16-hour .description").val(localStorage.getItem("16-hour"));
+jQuery("#17-hour .description").val(localStorage.getItem("17-hour"));
 
 // timeBlock();
 
-// LOOP THROUGH ALL ELEMENTS WHERE CLASS
-// = "row time-block"
+// LOOP THROUGH ALL ELEMENTS WHERE CLASS = "row time-block"
 
-// Then pass that elelment to function:
-function loopTimeElements() {
-    for (i = 8; i <  18; i ++) {
-        // TODO: get div id for all elements with class = "row time-block"
-    }
-};
+
+// // Then pass that element to function:
+// function loopTimeElements() {
+//     for (i = 9; i < 17; i++) {
+//         // TODO: get div id for all elements with class = "row time-block"
+//     }
+// };
